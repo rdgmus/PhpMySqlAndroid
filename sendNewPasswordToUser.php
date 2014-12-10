109e8e41e3eca6900b8e482914d9c83f1be3b759
rdgmus <rdgmus@live.com>
2014-12-10 19:28:13 +0100

    sendNewPasswordToUser.php
    server side scripting per
    RegistroAndroid v0.8-alpha

--- sendNewPasswordToUser.php
+++ sendNewPasswordToUser.php
@@ -19,22 +19,27 @@ $mySqlFunctions = new MySqlFunctionsClass();
 
 
 //INOLTRA RICHIESTA INSERENDO UN RECORD NELLA TABELLA change_password_request
-if (NULL != filter_input(INPUT_GET, 'email')) {
+if (NULL != filter_input(INPUT_GET, 'email')) {//String
   $email = filter_input(INPUT_GET, 'email');
 }else{
   echo (int) 0;
 }
 
-if (NULL != filter_input(INPUT_GET, 'password')) {
+if (NULL != filter_input(INPUT_GET, 'password')) {//String
   $password = filter_input(INPUT_GET, 'password');
 }else{
   echo (int) 0;
 }
 
+if (NULL != filter_input(INPUT_GET, 'forHimSelf')) {// Int :Se la password è per te stesso il messaggio è diverso
+  $forHimSelf = filter_input(INPUT_GET, 'forHimSelf');
+}else{
+  $forHimSelf = 0;
+}
 
-echo sendNewPasswordToUserWithEmail($email, $password);
+echo sendNewPasswordToUserWithEmail($email, $password, $forHimSelf);
 
-function sendNewPasswordToUserWithEmail($email, $password) {
+function sendNewPasswordToUserWithEmail($email, $password, $forHimSelf) {
   //INVIA EMAIL PER RICHIESTA CONFERMA
   $from = 'rdgmus@live.com';
   $to = $email;
@@ -42,8 +47,16 @@ function sendNewPasswordToUserWithEmail($email, $password) {
   $message_content = '<h2>Le inviamo la nuova password!</h2><br>' .
   "Le sue nuove credenziali sono:\n" .
   "<h1>Email: </h1><h2>".$email."</h2>\n" .
-  "<h1>Password: </h1><h2>".$password."</h2>\n" .
-  "Cordiali Saluti <br> Admin - RegistroAndroid <br>";
+  "<h1>Password: </h1><h2>".$password."</h2>\n\n";
+  if($forHimSelf == 0){
+    $message_content .=  "<h2>Le ricordiamo che al prossimo LOGIN,\n" .
+    "per una maggiore sicurezza del suo account,\n".
+    "deve cambiare nuovamente la sua password\n" .
+    "accedendo dal suo menu all'opzione 'Password'\n\n";
+  }
+  $message_content .=  "Cordiali Saluti <br> <h1>Admin - RegistroAndroid</h1> <br>";
+
+
   //Assigning a picture for {logo} replacement
   $logo = "images/cbasso1.png";
   //INVIA IL MESSAGGIO CON LE NUOVE CREDENZIALI ALL'UTENTE
